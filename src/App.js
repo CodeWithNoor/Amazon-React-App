01d188b52cb1ec78a75378ac38ce0d9ba1352e02
Noor <ma8073641@gmail.com>
2022-11-01 21:41:58 -0700

    #

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import Header from "./component/Header";
 import Home from "./component/Home";
+import Checkout from "./component/Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      {/* home */}
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Header />
+          <Home />
+          <Routes>
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/component/Checkout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/Checkout.css";
+
+const Checkout = () => {
+  return <div className="checkout"></div>;
+};
+
+export default Checkout;
